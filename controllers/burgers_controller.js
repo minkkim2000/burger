b07ddeb62e6a4849351def22bb1622078e956788
minkkim2000 <minkkim2000@gmail.com>
2018-02-24 19:31:09 -0800

    shows on page now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,18 +1,17 @@
 var express = require("express");
+
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-// // at /
-// router.get('/', function(req, res) {
-//     res.redirect('/index');
-// });
 
+router.get("/", function(req, res){
+    res.redirect("burgers");
+});
 
-
-router.get("/", function(req, res) {
+router.get("/burgers", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = { 
-            burgers: data
+            burger: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
@@ -21,21 +20,23 @@ router.get("/", function(req, res) {
 
 
 // route for creating burger
-router.post("/burgers/insertOne", function(req, res) {
-    burger.insertOne(
-        ["burger_name"], [req.body.name], function(data) {
-        res.redirect("/");
-    });
-});
-
-// route for updating burger
-router.put("burgers/updateOne:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    burger.updateOne({devoured: req.body.devoured}, condition, function() {
+router.post('/burgers/insertOne', function(req, res) {
+    burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function(res) {
         res.redirect('/');
     });
 });
 
+// route for updating burger
+router.put('/burgers/updateOne/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+    console.log('condition', condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(res) {
+        res.redirect('/');
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
